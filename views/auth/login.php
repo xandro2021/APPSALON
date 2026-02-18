5b6b0cfaae22979c02e44365c088b7f807198408
xandro2021 <xandrocastro2021@gmail.com>
2026-02-17 18:09:39 -0600

    creacion de html para login

---
+++ views/auth/login.php
@@ -0,0 +1,21 @@
+<h1 class="nombre-pagina">LOGIN</h1>
+<p class="descripcion-pagina">Inicia sesión con tus datos</p>
+
+<form method="POST" id="" action="/" class="formulario">
+    <div class="campo">
+        <label for="email"> Email </label>
+        <input type="email" id="email" placeholder="Tu Email" name="email" value="" />
+    </div>
+
+    <div class="campo">
+        <label for="password"> Password </label>
+        <input type="password" id="password" placeholder="Tu Password" name="password" value="" />
+    </div>
+
+    <input type="submit" class="boton" name="" value="Iniciar Sesion" />
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">Aún no tienes una cuenta? Crear una</a>
+    <a href="/olvide">Olvidaste tu password?</a>
+</div>
--- views/layout.php
+++ views/layout.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>App Salón</title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet"> 
-    <link rel="stylesheet" href="build/css/app.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700;900&display=swap" rel="stylesheet">
+
+    <link rel="preload" href="/build/css/app.min.css" as="style" />
+    <link rel="stylesheet" href="/build/css/app.min.css" type="text/css" media="screen" />
 </head>
+
 <body>
 
-    <?php echo $contenido; ?>
-            
+    <div class="contenedor-app">
+        <div class="imagen"> </div>
+
+        <div class="app">
+            <?php echo $contenido; ?>
+        </div>
+    </div>
+
 </body>
+
 </html>
