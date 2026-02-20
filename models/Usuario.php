fa7e64b274402ac26e617dcf9a50730566425b8f
xandro2021 <xandrocastro2021@gmail.com>
2026-02-20 12:15:10 -0600

    creacion del modelo usuario

---
+++ models/Usuario.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Model;
+
+use mysqli_result;
+
+class Usuario extends ActiveRecord
+{
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id', 'nombre', 'apellido', 'email', 'password', 'telefono', 'admin', 'confirmado', 'token'];
+
+    public $id, $nombre, $apellido, $email, $password, $telefono, $admin, $confirmado, $token;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->nombre = $args['nombre'] ?? '';
+        $this->apellido = $args['apellido'] ?? '';
+        $this->email = $args['email'] ?? '';
+        $this->password = $args['password'] ?? '';
+        $this->telefono = $args['telefono'] ?? '';
+        $this->admin = $args['admin'] ?? null;
+        $this->confirmado = $args['confirmado'] ?? null;
+        $this->token = $args['token'] ?? '';
+    }
+
+    /**
+     * @return array<string, string[]>
+     */
+    public function validar(): array
+    {
+        if (!$this->nombre) {
+            self::$alertas['error'][] = "El nombre es obligatorio";
+        }
+
+        if (!$this->apellido) {
+            self::$alertas['error'][] = "El apellido es obligatorio";
+        }
+
+        if (!$this->email) {
+            self::$alertas['error'][] = "El email es obligatorio";
+        }
+
+        if (!$this->password) {
+            self::$alertas['error'][] = "El password es obligatorio";
+        }
+
+        if (strlen($this->password) < 6) {
+            self::$alertas['error'][] = "El password debe contener al menos 6 caracteres";
+        }
+
+        return self::$alertas;
+    }
+
+    public function existeUsuario(): mysqli_result | bool
+    {
+        $query = "SELECT * FROM " . self::$tabla . " WHERE email = '$this->email' LIMIT 1;";
+        $resultado = self::$db->query($query);
+
+        if ($resultado->num_rows) {
+            self::$alertas['error'][] = "El usuario ya se encuentra registrado";
+        }
+
+        return $resultado;
+    }
+
+    public function hashPassword()
+    {
+        $this->password = password_hash($this->password, PASSWORD_BCRYPT);
+    }
+}
