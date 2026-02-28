a6c252b4513a1de48be67badd40a004e26ecc86c
xandro2021 <xandrocastro2021@gmail.com>
2026-02-28 12:01:06 -0600

    completar el crear citas

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -2,6 +2,8 @@
 
 namespace Controllers;
 
+use Model\Cita;
+use Model\CitaServicio;
 use Model\Servicio;
 
 class APIController
@@ -10,5 +12,29 @@ class APIController
     {
         $servicios = Servicio::all();
         echo json_encode($servicios);
+    }
+
+    public static function guardar()
+    {
+        // Almacena cita y devuelve id
+        $cita = new Cita($_POST);
+        $resultado = $cita->guardar();
+        $id = $resultado['id'];
+
+        // Almacena los servicios con el id de la cita
+        $idServicios = explode(',', $_POST['servicios']);
+
+        foreach ($idServicios as $idServicio) {
+            $args = [
+                'citaId' => $id,
+                'servicioId' => $idServicio
+            ];
+
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+        }
+
+        // Retorna la respuesta
+        echo json_encode([ 'resultado' => $resultado ]);
     }
 }
---
+++ views/templates/barra.php
@@ -0,0 +1,4 @@
+<div class="barra">
+    <p>Hola: <?= $nombre ?? '' ?></p>
+    <a class="boton" href="/logout">Cerrar Session</a>
+</div>
