2e01f4b028cff772c0f5a11d254299d1257a50fc
xandro2021 <xandrocastro2021@gmail.com>
2026-02-28 11:57:30 -0600

    usar la nueva barra reutilizable - template

--- views/cita/index.php
+++ views/cita/index.php
@@ -1,6 +1,8 @@
 <h1 class="nombre-pagina">Crear Nueva Cita</h1>
 <p class="descripcion-pagina"> Elige tus servicios y coloca tus datos </p>
 
+<?php include_once __DIR__ . "/../templates/barra.php"; ?>
+
 <div class="app">
 
     <nav class="tabs">
