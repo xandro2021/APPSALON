78e7f23661f14b9740b35be310b817acd6625a2c
xandro2021 <xandrocastro2021@gmail.com>
2026-03-01 17:49:25 -0600

    agregar servicios (CRUD)

--- models/Servicio.php
+++ models/Servicio.php
@@ -15,4 +15,24 @@ class Servicio extends ActiveRecord
         $this->nombre = $args['nombre'] ?? '';
         $this->precio = $args['precio'] ?? '';
     }
+
+    /**
+     * @return array<string, string[]>
+     */
+    public function validar(): array
+    {
+        if (!$this->nombre) {
+            self::$alertas['error'][] = "El nombre del servicio es obligatorio";
+        }
+
+        if (!$this->precio) {
+            self::$alertas['error'][] = "El precio del servicio es obligatorio";
+        }
+
+        if (!is_numeric($this->precio)) {
+            self::$alertas['error'][] = "El precio no es valido";
+        }
+
+        return self::$alertas;
+    }
 }
---
+++ views/servicios/crear.php
@@ -0,0 +1,14 @@
+<h1 class="nombre-pagina">Nuevo Servicio</h1>
+<p class="descripcion-pagina">Llena todos los campos para añadir un nuevo servicio</p>
+
+<?php
+include_once __DIR__ . "/../templates/barra.php";
+include_once __DIR__ . "/../templates/alertas.php";
+?>
+
+<form method="POST" class="formulario" action="/servicios/crear">
+
+    <?php include_once __DIR__ . "/formulario.php"; ?>
+
+    <input type="submit" class="boton" value="Guardar Servicio" />
+</form>
